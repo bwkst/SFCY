4b77a89459c9adc6c2f5e4528c0ef62c1c5ee445
bwkst <75024472+bwkst@users.noreply.github.com>
2021-01-11 00:19:07 +0800

    Project
    1

---
+++ Project/app/src/main/java/com/mainactivity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
